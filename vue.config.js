71f954e11054b98573192b053fa6f710a45db8bf
finish123 <704587172@qq.com>
2020-04-16 11:56:29 +0800

    项目结构划分,vue.config.js,editorconfig得复制

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    configureWebpack:{
+        resolve:{
+            // alias 别名得意思,方便后续得文件路径使用
+            alias:{
+                "assets": "@/assets",
+                "common":"@/common",
+                "components":"@/components",
+                "network":"@/network",
+                
+            }
+        }
+    }
+}
